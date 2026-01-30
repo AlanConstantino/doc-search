e0d2b4c128e8c5b6c91d52e53d22b21ea82d80d5
AlanConstantino <constantinoalan98@gmail.com>
2026-01-30 12:01:15 -0800

    chore: bump version to 1.6.0

--- doc_search/__init__.py
+++ doc_search/__init__.py
@@ -5,5 +5,5 @@ Search through large technical documentation websites (5k-15k pages)
 using BM25 ranking. Pure Python 3.9+ standard library, no dependencies.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __author__ = "Alan Constantino"
